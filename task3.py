f5631ffd4fe1e7f728496c285d252a33ee96b4e9
Jack <124203482+John-Smth@users.noreply.github.com>
2023-06-06 10:02:14 -0700

    Update task3.py

--- task3.py
+++ task3.py
@@ -5,27 +5,45 @@
 # a second, different image. Use the tkObject.after() method to animate it.
 # Note: You can find sprite sheets or tile sheets on the internet to help you!
 
+
 import tkinter as tk
-
+import itertools
+"""
+Task 1
+Read the map1.txt file and convert to a map that you can navigate a
+rectangle object through.
+"""
 w = tk.Tk()
-w.geometry("600x400")
-w.title("sample")
-
-c = tk.Canvas(width=550,height=450,background="#cccccc",bd="2")
+w.geometry("925x475")
+w.attributes('-topmost',True)
+c = tk.Canvas(height=475,width=900,bg="#ffdddd")
 c.pack()
+f = open('map1.txt')
+fr=f.read()
+frs=fr.split('\n')
+print(frs)
 
 
-rec = c.create_rectangle(50,50,80,80,fill="#aa0000")
+maze=[]
 
 
-def keyPress(e):
-    print(e)
-    print(e.keycode, e.keysym, e.x, e.y)
-    
-w.bind("<Left>",keyPress)
-w.bind("<Right>",keyPress)
-w.bind("<Up>",keyPress)
-w.bind("<Down>",keyPress)
+for k in range(0,15):
+    for (i,j) in itertools.zip_longest(frs[k],range(len(frs[k]))):
+        if i=='*':
+            maze.append(c.create_rectangle(0+j*31,0+k*31,31+j*31,31+k*31,fill='#000000'))
+        else:
+            pass
+
+
+
+
+'''
+1-29
+30-59
+etc
+'''
+
+
 
 
 w.mainloop()
